6fce6f8b35beef7e1ea17966c96c866639295202
fume <fume90@hotmail.com>
2016-11-05 02:37:49 +0100

    exposing via NgModule

--- src/core.ts
+++ src/core.ts
@@ -1,10 +1,2 @@
-import {AdalService, AuthHttp} from './services';
-
 export * from './services';
-
-export const ANGULAR2_ADAL_PROVIDERS: any[] = [
-    {
-        provide: AdalService,
-        useClass: AdalService
-    }
-];
+export * from './modules';
