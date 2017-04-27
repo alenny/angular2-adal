4242c5cd5951822ca8a9a6f99b91a5873f342453
Magnus Gudmundsson <cyberdada@cyberdada.com>
2017-04-27 16:09:06 +0200

    changed import from rxjs

--- src/services/adal.service.ts
+++ src/services/adal.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable } from 'rxjs/Observable';
 import * as adalLib from 'adal-angular';
 import { OAuthData } from "./oauthdata.model";
 import User = adal.User;
