f5c66edd641fac8cae3781e625ca391d9e6c39eb
Chahal <Suresh.Chahal@edelman.com>
2017-05-17 10:50:54 -0500

    Reverted pull from Cyberdada , it broke build

--- src/services/adal.service.ts
+++ src/services/adal.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import * as adalLib from 'adal-angular';
 import { OAuthData } from "./oauthdata.model";
 import User = adal.User;
