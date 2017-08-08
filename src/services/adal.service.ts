2c941727428691265c4a28c507f1abc8f91e1347
Anthony Ciccarello <aciccarello@users.noreply.github.com>
2017-08-08 16:53:21 -0600

    Eliminate dead code in adal.service
    The user object is always being defaulted so if (user) will always be true.

--- src/services/adal.service.ts
+++ src/services/adal.service.ts
@@ -166,16 +166,9 @@ export class AdalService {
         let token = this.adalContext.getCachedToken(resource);
         this.oauthData.isAuthenticated = token !== null && token.length > 0;
         let user = this.adalContext.getCachedUser() || { userName: '', profile: undefined };
-        if (user) {
-            this.oauthData.userName = user.userName;
-            this.oauthData.profile = user.profile;
-            this.oauthData.loginError = this.adalContext.getLoginError();
-        }
-        else {
-            this.oauthData.userName = '';
-            this.oauthData.profile = {};
-            this.oauthData.loginError = '';
-        }
+        this.oauthData.userName = user.userName;
+        this.oauthData.profile = user.profile;
+        this.oauthData.loginError = this.adalContext.getLoginError();
 
     };
 }
