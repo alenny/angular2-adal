45846df05ecdb1ce46a1502a54dffc59e7a8d4ef
wijn0038 <r.j.d.wijnen@hz.nl>
2017-11-30 08:21:33 +0100

    Update add interceptor instead of AuthHttp

---
+++ src/interceptors.ts
@@ -0,0 +1,1 @@
+export {AuthHttpInterceptor} from './interceptors/authHttp.interceptor';
--- src/modules/authHttp.module.ts
+++ src/modules/authHttp.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 
 import { AdalModule } from './adal.module';
-import { AuthHttp } from './../services/authHttp.service'
+import { HTTP_INTERCEPTORS } from '@angular/common/http'
+import { AuthHttpInterceptor } from './../interceptors/authHttp.interceptor'
 
 @NgModule({
     imports: [AdalModule],
     exports: [],
     declarations: [],
-    providers: [AuthHttp],
+    providers: [ {
+        provide:  HTTP_INTERCEPTORS,
+        useClass: AuthHttpInterceptor,
+        multi: true
+    }],
 })
 export class AuthHttpModule { }
