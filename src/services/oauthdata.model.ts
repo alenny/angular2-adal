046e977e5e060d37287aac456d053dc1d0945c44
Achim Strauss <acstrauss@munichre.com>
2018-01-12 14:58:57 +0100

    Add authenticationStatus observable to OAuthData

--- src/services/oauthdata.model.ts
+++ src/services/oauthdata.model.ts
@@ -1,5 +1,8 @@
+import { Observable } from "rxjs/Observable";
+
 export interface OAuthData {
     isAuthenticated: boolean;
+    authenticationStatus: Observable<boolean>;
     userName: string;
     loginError: string;
     profile: any;
